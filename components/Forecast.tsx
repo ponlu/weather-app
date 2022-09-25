ba60214045f6ca640b4f38cd09bb529e911534ee
ponlu <pontuslundin@hotmail.com>
2022-09-25 19:49:01 +0200

    [9] - Created skeleton for smaller component to display upcoming forecasts

--- components/Forecast.tsx
+++ components/Forecast.tsx
@@ -1,14 +1,18 @@
 import { GetServerSideProps } from "next";
 import { useEffect, useState } from "react";
-import { ICoord, IForecastResponse, IList } from "../interfaces/IForecastResponse";
+import { ICity, ICoord, IForecastResponse, IList } from "../interfaces/IForecastResponse";
 import { getForecast } from "../lib/server/OpenWeatherAPI";
 
-function Forecast({ forecasts }: { forecasts: IList[] }) {
+function Forecast({ forecasts, city }: { forecasts: IList[]; city: ICity }) {
   const stockholmCoord: ICoord = { lat: 59.33, lon: 18.06 };
   const [coordinates, setCoordinates] = useState<ICoord>(stockholmCoord); // Default to Stockholm
   const [selectedForecast, setSelectedForecast] = useState<IForecastResponse>();
 
-  return <div className="flex place-items-center "></div>;
+  return (
+    <div className="bg-orange-500 flex outline rounded-lg">
+      <p>{forecasts[0].dt_txt.toString()}</p>
+    </div>
+  );
 }
 
 export default Forecast;
