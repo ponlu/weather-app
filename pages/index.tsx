7c98b743baf87c6a4e43c35c566d96c654a5d32c
ponlu <pontuslundin@hotmail.com>
2022-09-26 20:42:58 +0200

    [5] - Corrected wrongful css alignment

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,13 @@ import WeatherGrid from "../components/WeatherGrid";
 
 const Home: NextPage = () => {
   return (
-    <div className="h-screen w-screen overflow-auto bg-gray-900 p-2">
+    <div className="h-screen w-screen overflow-auto bg-gray-900 p-2 flex justify-center">
       <Head>
         <title>Weather App</title>
         <link rel="icon" href="/images/Clear.png" />
       </Head>
 
-      <main className="flex justify-center w-full md:w-144 lg:w-144">
+      <main className="w-full md:w-144 lg:w-144">
         <WeatherGrid />
       </main>
     </div>
