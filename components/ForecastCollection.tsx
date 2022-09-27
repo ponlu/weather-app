0945abf38553fb0eeab4309618d90f5185cc2c12
ponlu <pontuslundin@hotmail.com>
2022-09-27 21:49:52 +0200

    Debug

--- components/ForecastCollection.tsx
+++ components/ForecastCollection.tsx
@@ -5,12 +5,16 @@ import Forecast from "./Forecast";
 
 function ForecastCollection({ forecastResponse }: { forecastResponse: IForecastResponse }) {
   const day1 = new Date(forecastResponse.list[0].dt_txt);
+
+  forecastResponse.list.map((forecast, index) => {
+    if (!forecast) {
+      console.error(`FEL I FORECAST NR: ${index}`);
+    }
+  });
+
   const upcomingForecast: IForecastList[][] = [
     [forecastResponse.list[0]],
     [forecastResponse.list[10]],
-    [forecastResponse.list[20]],
-    [forecastResponse.list[30]],
-    [forecastResponse.list[39]],
     // forecastResponse.list.filter((forecast) => new Date(forecast.dt_txt).getDate() === day1.getDate()),
     // forecastResponse.list.filter((forecast) => new Date(forecast.dt_txt).getDate() === day1.getDate()),
     // forecastResponse.list.filter((forecast) => new Date(forecast.dt_txt).getDate() === addDays(day1, 1).getDate()),
